2757f2871ea2e752d39d85c1c271034462fc48bb
Chen Fengyuan <chenfengyuan@outlook.com>
2018-03-20 15:29:19 +0800

    build: update

--- rollup.config.js
+++ rollup.config.js
@@ -4,36 +4,7 @@ const nodeResolve = require('rollup-plugin-node-resolve');
 const pkg = require('./package');
 
 const now = new Date();
-
-export default {
-  input: 'src/index.js',
-  output: [
-    {
-      file: 'dist/vue-countdown.js',
-      format: 'umd',
-    },
-    {
-      file: 'dist/vue-countdown.common.js',
-      format: 'cjs',
-    },
-    {
-      file: 'dist/vue-countdown.esm.js',
-      format: 'es',
-    },
-    {
-      file: 'docs/js/vue-countdown.js',
-      format: 'umd',
-    },
-  ],
-  name: 'VueCountdown',
-  plugins: [
-    nodeResolve(),
-    commonjs(),
-    babel({
-      exclude: 'node_modules/**',
-    }),
-  ],
-  banner: `/*!
+const banner = `/*!
  * vue-countdown v${pkg.version}
  * https://github.com/${pkg.repository}
  *
@@ -42,5 +13,38 @@ export default {
  *
  * Date: ${now.toISOString()}
  */
-`,
+`;
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      banner,
+      file: 'dist/vue-countdown.js',
+      format: 'umd',
+      name: 'VueCountdown',
+    },
+    {
+      banner,
+      file: 'dist/vue-countdown.common.js',
+      format: 'cjs',
+    },
+    {
+      banner,
+      file: 'dist/vue-countdown.esm.js',
+      format: 'es',
+    },
+    {
+      banner,
+      file: 'docs/js/vue-countdown.js',
+      format: 'umd',
+      name: 'VueCountdown',
+    },
+  ],
+  plugins: [
+    babel({
+      exclude: 'node_modules/**',
+      plugins: ['external-helpers'],
+    }),
+  ],
 };
